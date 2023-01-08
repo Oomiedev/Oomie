a71e6f8afc5547206b8d7d82e5758a395c84bba2
Nurlan Akylbekov <n.akylbekov@vlife.kz>
2023-01-08 20:35:00 +0600

    add onboarding, add session tracker

--- HollySounds/AppDelegate.swift
+++ HollySounds/AppDelegate.swift
@@ -8,11 +8,16 @@
 import UIKit
 import AFKit
 import AVFoundation
+import OomieOnboarding
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+  
+  var sessionTracker: SessionTracker {
+    return SessionTrackerImpl(storage: UserDefaultsStorage())
+  }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -34,19 +39,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          */
         
         DataManager.shared.initialize { [weak self] in
-            
-            self?.window?.layer.add(
+            guard let self = self else { return }
+            self.window?.layer.add(
                 CATransition(),
                 forKey: nil
             )
             
-            let viewController = GalleryViewController()
-            let navigationController = AFDefaultNavigationController(rootViewController: viewController)
-            self?.window?.rootViewController = navigationController
+          self.window?.rootViewController = self.sessionTracker.isFirstLaunch ? self.createOnboardingScreen() : self.createHomeScreen()
         }
         
         return true
     }
 
+  private func createOnboardingScreen() -> UIViewController {
+    let onboarding = OnboardingViewController()
+    onboarding.delegate = self
+    return onboarding
+  }
+  
+  private func createHomeScreen() -> UIViewController {
+    let viewController = GalleryViewController()
+    return AFDefaultNavigationController(rootViewController: viewController)
+  }
 }
 
+extension AppDelegate: OnboardingViewControllerDelegate {
+  func finishOnboarding() {
+    guard let window = self.window else { return }
+    
+    sessionTracker.isFirstLaunch = false
+    
+    let transition: () -> Void = { [weak self] in
+      window.rootViewController = self?.createHomeScreen()
+    }
+    
+    if let previousController = window.rootViewController {
+      UIView.transition(with: window, duration: 0.5, options: [.beginFromCurrentState, .transitionFlipFromRight], animations: transition)
+      previousController.dismiss(animated: false) {
+        previousController.view.removeFromSuperview()
+      }
+    } else {
+      transition()
+    }
+  }
+}
