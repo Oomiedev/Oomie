969b2ad54b12309d94fa71015e61c6170c5460cd
Nurlan Akylbekov <n.akylbekov@vlife.kz>
2023-01-18 23:39:23 +0600

    prepare networking service

---
+++ HollySounds/Services/NetworkingService/NetworkingService.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkingService.swift
+//  HollySounds
+//
+//  Created by Nurlan Akylbekov  on 18.01.2023.
+//
+
+import Foundation
+
+protocol NetworkingService: AnyObject {
+  
+}
+
+final class NetworkingServiceImpl {
+  
+}
+
+extension NetworkingServiceImpl: NetworkingService {
+  
+}
